dfd10a8c66b2ac7a8c9c4b66e0af9a21b03b6ffd
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2011-10-04 14:47:39 +0200

    Added unittestbutton and dummy actions
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- src/com/honei/HoneiActivity.java
+++ src/com/honei/HoneiActivity.java
@@ -2,14 +2,27 @@ package com.honei;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View.*;
+import android.view.*;
+import android.widget.Button;
 
-public class HoneiActivity extends Activity
+public class HoneiActivity extends Activity implements OnClickListener
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    //get button and set listener
+    Button button = (Button)findViewById(R.id.button);
+    button.setOnClickListener(this);
+
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+  public void onClick(View v)
+  {
+
+  }
+
 }
